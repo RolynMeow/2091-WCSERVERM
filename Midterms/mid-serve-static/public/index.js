609704f64d457ba8834392bb89cb1b642da516bf
RolynMeow <122800669+RolynMeow@users.noreply.github.com>
2023-02-20 23:29:33 +0800

    1 file modified

--- Midterms/mid-serve-static/public/index.js
+++ Midterms/mid-serve-static/public/index.js
@@ -7,9 +7,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/' + 'index.html');
 });
 
-app.all('*', (req, res) => {
-  res.status(404).send('<h1>Error 404 page not found.</h1>');
-});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
